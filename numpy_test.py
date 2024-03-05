65585299e0e379049711af688d1ddeb074960a76
mhogg <mhogg@student.42.fr>
2024-03-05 16:33:53 +0100

    fix one-dimentional matrix

--- numpy_test.py
+++ numpy_test.py
@@ -2,5 +2,8 @@ import numpy as np
 
 x = np.array([[1, 2, 3, 4], [1, 2, 3, 4]])
 print(x.shape)
-
 print(x)
+
+y = np.array([1, 2, 3, 4])
+print(y.shape)
+print(y)
--- vector.py
+++ vector.py
@@ -15,38 +15,58 @@ class Vector:
     
     # A function to reshape a vector into a matrix
     def reshape(self):
-        return(Matrix(self))
+        return(Matrix(self.vect))
 
 
 
 class Matrix:
+
+    rows = 0
+    columns = 0
 
     # Usage: m = Matrix([[1, 2], [3, 4]])
     def __init__(self, matr: list):
        
         # if type(matr[0]) is list:
         self.matr = matr
-        # one-dimentional matrix
-        # else:
-        #     self.matr = []
-        #     self.matr.append(matr)
+        self.rows = len(self.matr)
+        try:
+            self.columns = len(self.matr[0]) # one-dimentional matrix
+        except:
+            columns = 0
 
     
     # A function to return the shape of a matrix
     # returnes tuple of array dimentions
     def shape(self):
-        return(len(self.matr), len(self.matr[0]))
-    
+        # self.rows = len(self.matr)
+        # try:
+        #     self.columns = len(self.matr[0])
+        # except:
+        #     print(self.rows)
+        #     print(self.columns)
+        #     return(self.rows,)
+        # print(self.rows)
+        # print(self.columns)
+        # return(self.rows,self.columns)
+        if self.columns == 0:
+            return (self.rows,)
+        return(self.rows,self.columns)
     # A function to print a matrix on the standard output
     def print(self):
-        print('[', end='')
-        print(self.matr[0])
-        i = 1
-        while i < len(self.matr)-1:
-            print('', self.matr[i])
-            i = i+1
-        print('', self.matr[i], end='')
-        print(']')
+        print(self.rows)
+        print(self.columns)
+        if (self.columns) == 0:
+            print(self.matr)
+        else:
+            print('[', end='')
+            print(self.matr[0])
+            i = 1
+            while i < self.rows-1:
+                print('', self.matr[i])
+                i = i+1
+            print('', self.matr[i], end='')
+            print(']')
     
     # A function to reshape a matrix into a vector
     def reshape(self):
