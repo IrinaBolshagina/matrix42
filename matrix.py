bf597d609847e6ae6bfc615cf1fc0c6b34abf712
Madelaine Hogg <mhogg@paul-f4Ar5s3.clusters.42paris.fr>
2024-03-10 18:54:32 +0100

    separated matrix.py

---
+++ matrix.py
@@ -0,0 +1,56 @@
+from vector import Vector
+
+class Matrix:
+
+    rows = 0
+    columns = 0
+
+    # Usage: m = Matrix([[1, 2], [3, 4]])
+    def __init__(self, matr: list):
+       
+        # if type(matr[0]) is list:
+        self.matr = matr
+        self.rows = len(self.matr)
+        try:
+            self.columns = len(self.matr[0]) # one-dimentional matrix
+        except:
+            columns = 0
+
+    
+    # A function to return the shape of a matrix
+    # returnes tuple of array dimentions
+    def shape(self):
+        # self.rows = len(self.matr)
+        # try:
+        #     self.columns = len(self.matr[0])
+        # except:
+        #     print(self.rows)
+        #     print(self.columns)
+        #     return(self.rows,)
+        # print(self.rows)
+        # print(self.columns)
+        # return(self.rows,self.columns)
+        if self.columns == 0:
+            return (self.rows,)
+        return(self.rows,self.columns)
+    # A function to print a matrix on the standard output
+    def print(self):
+        print(self.rows)
+        print(self.columns)
+        if (self.columns) == 0:
+            print(self.matr)
+        else:
+            print('[', end='')
+            print(self.matr[0])
+            i = 1
+            while i < self.rows-1:
+                print('', self.matr[i])
+                i = i+1
+            print('', self.matr[i], end='')
+            print(']')
+    
+    # A function to reshape a matrix into a vector
+    def reshape(self):
+        vect = [element for row in self.matr for element in row]
+        # return(vect)
+        return(Vector(vect))
--- vector.py
+++ vector.py
@@ -15,6 +15,7 @@ class Vector:
     
     # A function to reshape a vector into a matrix
     def reshape(self):
+        from matrix import Matrix
         return(Matrix(self.vect))
 
     # Print overload
@@ -45,60 +46,3 @@ class Vector:
     def __rmul__(self, other):
         mul_vect = [self.vect[i]*other for i in range(len(self.vect))]
         return Vector(mul_vect)
-
-
-
-class Matrix:
-
-    rows = 0
-    columns = 0
-
-    # Usage: m = Matrix([[1, 2], [3, 4]])
-    def __init__(self, matr: list):
-       
-        # if type(matr[0]) is list:
-        self.matr = matr
-        self.rows = len(self.matr)
-        try:
-            self.columns = len(self.matr[0]) # one-dimentional matrix
-        except:
-            columns = 0
-
-    
-    # A function to return the shape of a matrix
-    # returnes tuple of array dimentions
-    def shape(self):
-        # self.rows = len(self.matr)
-        # try:
-        #     self.columns = len(self.matr[0])
-        # except:
-        #     print(self.rows)
-        #     print(self.columns)
-        #     return(self.rows,)
-        # print(self.rows)
-        # print(self.columns)
-        # return(self.rows,self.columns)
-        if self.columns == 0:
-            return (self.rows,)
-        return(self.rows,self.columns)
-    # A function to print a matrix on the standard output
-    def print(self):
-        print(self.rows)
-        print(self.columns)
-        if (self.columns) == 0:
-            print(self.matr)
-        else:
-            print('[', end='')
-            print(self.matr[0])
-            i = 1
-            while i < self.rows-1:
-                print('', self.matr[i])
-                i = i+1
-            print('', self.matr[i], end='')
-            print(']')
-    
-    # A function to reshape a matrix into a vector
-    def reshape(self):
-        vect = [element for row in self.matr for element in row]
-        # return(vect)
-        return(Vector(vect))
